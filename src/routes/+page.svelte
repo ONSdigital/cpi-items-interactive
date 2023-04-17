0d4f4b820172b1f0614b722ea5eefec0464a8fef
henryjameslau <henry.james.lau@gmail.com>
2023-04-17 22:18:41 +0100

    switch to search and details

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,7 +14,7 @@
 	import Embed from "../components/helpers/Embed.svelte"
 	import Feedback from "../components/helpers/Feedback.svelte"
 	import pym from "pym.js";
-	import Scrolly from "../components/helpers/Scrolly.svelte"
+	import Fuzzy from "svelte-fuzzy";
 
 	let items; //metadata
 	let itemsSorted
@@ -35,6 +35,8 @@
 	let mainElementHeight;
 	let lastmonth;
 	let value;
+	let query;
+	let formatted = [];
 
 	onMount(async () => {
 			(items = await csv(
@@ -129,6 +131,13 @@
 		updateHeight()
 	}
 
+	function clearInput(){
+		filter=""
+	}
+	function input(){
+		typing = true;
+		setTimeout(()=>{typing=false;},200)
+	}
 
 
 	function updateHeight(event) {
@@ -167,6 +176,24 @@
 		}
 	});
 
+	$: if(filter) regex = new RegExp(filter, "gi")
+
+	$: if(filter){
+		grouped = groups(itemsSorted,d=>d.Category1,d=>d.Category2)
+		grouped = grouped.filter(d=>(d[1].some(e=>e[0].match(regex))) || (d[1].some(e=>e[1].some(f=>f.ITEM_DESC.match(regex)))))
+		.map(function(d){
+		 	if(d[1].some(e=>e[0].match(regex))){return [d[0],d[1].filter(e=>e[0].match(regex))]}
+			else{
+				return [d[0],d[1].filter(e=>e[1].some(f=>f.ITEM_DESC.match(regex))).filter(e=>e[1].filter(f=>f.ITEM_DESC.match(regex)))]
+			}
+		}) 
+		console.log(grouped)
+	}
+	$:if(itemsSorted&&!filter){
+		grouped = groups(itemsSorted,d=>d.Category1,d=>d.Category2)
+	}
+
+
 </script>
 <main>
 
@@ -174,69 +201,82 @@
 	<div id='title'>
 		<h1>How is the average price of items changing over time?</h1>
 		<p>Use our interactive to see how the price of different items have changed since last year.</p>
+		<hr class="white"/>
+	</div>
+	
 
+	<div class="input">
+		<h2>Shopping items search</h2>
+		<p>Search for items from the list to add to your shopping basket.</p>
+
+		<input bind:value={query}/>
+		<button on:click={() => (query = "")}>Clear</button>
+
+		<Fuzzy {query} data={items} options={{keys:["ITEM_DESC"]}} bind:formatted />
+
+		{#each formatted.slice(0,10) as item}
+			{#each item as line}
+				<li>
+				{#each line as { matches, text }}
+					{#if matches}
+					<mark>{text}</mark>
+					{:else}
+					{text}
+					{/if}
+				{/each}
+				</li>
+			{/each}
+		{/each}
+		<hr class="white"/>
+
+		<h2>Shopping items list</h2>
+		<p>Choose items from our shopping list to add them to your basket.</p>
 	</div>
 
-	<div id="input">
-	<h2>Add items to your basket</h2>
-	<p>Select items to add them to your basket.</p>
-		<!-- {#if grouped}
-		{#each grouped as group}
-			<details>
-				<summary>{group[0]}</summary>
-				{#each group[1] as subgroup}
-					<details class='subgroup'>
-						<summary>{subgroup[0]}</summary>
-						<div class='flex'>
-						{#each subgroup[1] as item(item.ITEM_ID)}
-							<div>
-								<input type="checkbox" id={item.ITEM_ID} on:change={handleChange} bind:checked={isChecked[item.ITEM_ID]} />
-								<label for={item.ITEM_ID}>
-									<span class="bold">{item.ITEM_DESC}</span>
-									{item['WEIGHT\\SIZE'] ? item['WEIGHT\\SIZE'] : ''}
-								</label>
-							</div>
-						{/each}
-						</div>
-					</details>
-				{/each}
-			</details>
-		{/each}
-		{/if} -->
-		{#if grouped}
-		<div id="allitems">
-		<section id="scrolly">
+	<div id='receiptheader'>
+		<svg viewBox="0 0 360 6">
+			<path d="M 0,6 L 12,0 L 24,6 L 36,0 L 48,6 L 60,0 L 72,6 L 84,0 L 96,6 L 108,0 L 120,6 L 132,0 L 144,6 L 156,0 L 168,6 L 180,0 L 192,6 L 204,0 L 216,6 L 228,0 L 240,6 L 252,0 L 264,6 L 276,0 L 288,6 L 300,0 L 312,6 L 324,0 L 336,6 L 348,0 L 360,6" fill="#F8FAFC"></path>
+		  </svg>
+	</div>
 
-			{#if value}<h3><span>{grouped[value][0]}</span></h3>
-			{:else}<h3><span>{grouped[0][0]}</span></h3>
-			{/if}
-			<Scrolly bind:value>
-				{#each grouped as groups, i}
-					{@const active = value === i}
-					<div class="step" class:active>
-						<h4>{groups[0]}</h4>
-						{#each groups[1] as subgroup}
-							<h5>{subgroup[0]}</h5>
-							<div class="hflex">
-							{#each subgroup[1].filter(e=>!selected.map(e=>+e[0]).includes(e.ITEM_ID)) as item(item.ITEM_ID)}
-								<div class="item" in:receive="{{key: item.ITEM_ID}}" out:send="{{key: item.ITEM_ID}}">
+	<div class="input receiptbackground">
 
-									<input type="checkbox" id={item.ITEM_ID} on:change={handleChange} bind:checked={isChecked[item.ITEM_ID]} />
-									<label for={item.ITEM_ID}
-										><span class="bold">{item.ITEM_DESC}</span>
-										{item['WEIGHT\\SIZE'] ? item['WEIGHT\\SIZE'] : ''}</label
-									>
-								</div>
+<!-- 	
+		<div>
+			<label for="filter">Filter items:</label><input id="filter" type=text bind:value={filter} on:input={input}/><button on:click={clearInput}>Clear filter</button>
+		</div> -->
+		
+			<div id="allitems">
+				{#if grouped}
+					{#each grouped as groups, i}
+						<details open={i==0}>
+						<summary><h2>{groups[0]}</h2>
+							<span class="ons-details__icon">
+								<svg class="ons-svg-icon" viewBox="0 0 8 13" xmlns="http://www.w3.org/2000/svg" focusable="false" fill="currentColor"><path d="M5.74,14.28l-.57-.56a.5.5,0,0,1,0-.71h0l5-5-5-5a.5.5,0,0,1,0-.71h0l.57-.56a.5.5,0,0,1,.71,0h0l5.93,5.93a.5.5,0,0,1,0,.7L6.45,14.28a.5.5,0,0,1-.71,0Z" transform="translate(-5.02 -1.59)"></path></svg>
+							</span>	
+							</summary>	
 							
-							{/each}
+						{#each groups[1] as subgroup}
+							<h3>{subgroup[0]}</h3>
+							<div class="hflex">
+								{#each subgroup[1].filter(e=>!selected.map(e=>+e[0]).includes(e.ITEM_ID)) as item(item.ITEM_ID)}
+									<div class="item" in:receive="{{key: item.ITEM_ID}}" out:send="{{key: item.ITEM_ID}}">
+
+										<input type="checkbox" id={item.ITEM_ID} on:change={handleChange} bind:checked={isChecked[item.ITEM_ID]} />
+										<label for={item.ITEM_ID}
+											><span class="bold">{item.ITEM_DESC}</span>
+											{item['WEIGHT\\SIZE'] ? item['WEIGHT\\SIZE'] : ''}</label
+										>
+									</div>
+								
+								{/each}
 							</div>
 						{/each}
-					</div>
-				{/each}
-			</Scrolly>
-		</section>
-		</div>
-		{/if}
+						</details>
+					{/each}
+				{/if}
+			</div>
+
 	</div>
 </div>
 
@@ -278,40 +318,82 @@
 </main>
 
 <style>
-	.spacer {
-		height: 75vh;
+	*{
+		box-sizing: border-box;
 	}
 
-	h3 {
-		position: sticky;
-		top: 0.5em;
+	#receiptheader{
+		overflow: hidden;
+		height:17px;
 	}
 
+	.receiptbackground{
+		background-color: #F8FAFC;
+	}
 
 	div#top{
 		background-color: #E9EFF4;
 	}
 
-	div#title, div#input{
+	div#title, div.input{
 		padding:25px;
+		padding-bottom: 0;
 	}
 
-	:global(span.highlight){
-		background-color: #f0f762;
+	.white{
+		border:2px solid white;
 	}
 
-	.subgroup{
-		margin-left:10px;
+	summary{
+		border-top: 1px solid #707071;
+		margin:0;
+		padding-bottom: .9rem;
+		padding-top: 1rem;
+		width:100%;
+		color: #206095;
+		cursor: pointer;
+		display:inline-block;
+		outline:none;
+		padding-left: 1.5rem;
+		pointer-events: initial;
+		position: relative;
 	}
 
-	.flex {
-		display: flex;
-		flex-flow: wrap;
-		flex-direction: column;
+	summary h2{
+		line-height: 1.4;
+		margin: 0 1rem 0 0;
+		display: inline-block;
+		font-size: 1rem;
+		font-weight: 700;
+		text-underline-position: under;
+		transform:translateY(-1px)
 	}
+
+	.ons-details__icon{
+	top: .8rem;
+	display: inline-block;
+	fill: var(--ons-color-text-link);
+	height: 1.5rem;
+	left: -.15rem;
+	position: absolute;
+	width: 1.5rem;
+}
+
+.ons-svg-icon {
+	height: 1rem;
+	vertical-align: middle;
+	width: 1rem;
+}
+
+details[open] .ons-details__icon {
+	left: -.1rem;
+	top: 1.2rem;
+	-webkit-transform: rotate(90deg);
+	transform: rotate(90deg);
+}
 
 	.bold {
-		font-weight: 700;
+		font-weight: 600;
 	}
 
 	.hflex{
@@ -319,15 +401,19 @@
 		flex-flow: wrap;
 	}
 
+	.hflex:last-child{
+		margin-bottom: 20px;
+	}
+
 	div#allitems {
 		height: 400px;
 		overflow-y: scroll;
 		position: relative;
-		text-align: center;
+		text-align: left;
 		padding-top: 2em;
-		box-sizing: border-box;
-		background-color: white;
+		background-color: #F8FAFC;
 		margin-top: 10px;
+
 	}
 
 
@@ -343,6 +429,33 @@
 		padding: 25px;
 	}
 
+	.item{
+		position: relative;
+	}
+
+	input[type=checkbox] {
+		-webkit-appearance: none;
+		-moz-appearance: none;
+		appearance: none;
+		background-color: var(--ons-color-input-bg);
+		border: 2px solid #222;
+		border-radius: .2rem;
+		cursor: pointer;
+		height: 22px;
+		left: 0;
+		top: 0;
+		width: 22px;
+		position:absolute;
+		z-index: 1;
+	}
+
+	.item label{
+		padding: 0 10px 0 33px;
+		cursor: pointer;
+		display: block;
+	width: 100%;
+
+	}
 
 	#results >h2{
 		color: #206095;
@@ -352,6 +465,15 @@
         margin:0;
     }
 
+
+	h3{
+		font-family: Open Sans;
+		font-size: 18px;
+		font-weight: 700;
+		line-height: 25px;
+		letter-spacing: 0em;
+		text-align: left;
+	}
 
 	.gap-y-0\.5 {
 		row-gap: .125rem;
@@ -367,8 +489,106 @@
 	.flex-wrap {
 		flex-wrap: wrap;
 	}
-	.flex {
-		display: flex;
-	}
 
+	:root {
+	--ons-color-black: #222;
+	--ons-color-grey-100: #414042;
+	--ons-color-grey-75: #707071;
+	--ons-color-grey-35: #bcbcbd;
+	--ons-color-grey-25: #d5d5d6;
+	--ons-color-grey-15: #e2e2e3;
+	--ons-color-grey-5: #f5f5f6;
+	--ons-color-white: #fff;
+	--ons-color-night-blue: #003c57;
+	--ons-color-spring-green: #a8bd3a;
+	--ons-color-ocean-blue: #206095;
+	--ons-color-sky-blue: #27a0cc;
+	--ons-color-aqua-teal: #00a3a6;
+	--ons-color-leaf-green: #0f8243;
+	--ons-color-ruby-red: #d0021b;
+	--ons-color-jaffa-orange: #fa6401;
+	--ons-color-sun-yellow: #fbc900;
+	--ons-color-neon-yellow: #f0f762;
+	--ons-color-ocean-blue-tint: #e9eff4;
+	--ons-color-ocean-blue-vibrant: #1f8fd8;
+	--ons-color-spring-green-tint: #c3c5b8;
+	--ons-color-leaf-green-tint: #e7f3ec;
+	--ons-color-leaf-green-vibrant: #10ca64;
+	--ons-color-leaf-green-dark-10: #073d20;
+	--ons-color-leaf-green-dark-5: #0c6b37;
+	--ons-color-ruby-red-tint: #fae6e8;
+	--ons-color-ruby-red-vibrant: #fd112d;
+	--ons-color-jaffa-orange-tint: #fff0e6;
+	--ons-color-jaffa-orange-vibrant: #ff7b24;
+	--ons-color-sun-yellow-dark: #e2b500;
+	--ons-color-branded: var(--ons-color-ocean-blue);
+	--ons-color-branded-text: var(--ons-color-ocean-blue);
+	--ons-color-branded-tint: var(--ons-color-ocean-blue-tint);
+	--ons-color-branded-secondary: var(--ons-color-night-blue);
+	--ons-color-branded-tertiary: var(--ons-color-aqua-teal);
+	--ons-color-branded-supporting: var(--ons-color-spring-green);
+	--ons-color-branded-supporting-tint: var(--ons-color-branded-supporting);
+	--ons-color-header: var(--ons-color-branded);
+	--ons-color-header-neutral: var(--ons-color-white);
+	--ons-color-header-masthead: var(--ons-color-white);
+	--ons-color-header-masthead-internal: var(--ons-color-branded-secondary);
+	--ons-color-header-masthead-neutral: var(--ons-color-black);
+	--ons-color-header-title: var(--ons-color-text);
+	--ons-color-header-navigation-links: var(--ons-color-text);
+	--ons-color-service-links: var(--ons-color-white);
+	--ons-color-cta-bg: var(--ons-color-branded-tint);
+	--ons-color-banner-bg: var(--ons-color-grey-5);
+	--ons-color-banner-bg-dark: var(--ons-color-grey-15);
+	--ons-color-page-light: var(--ons-color-white);
+	--ons-color-banner-browser-bg: var(--ons-color-black);
+	--ons-color-hero-bg: var(--ons-color-branded-tint);
+	--ons-color-hero-bg-dark: var(--ons-color-branded);
+	--ons-color-text: var(--ons-color-black);
+	--ons-color-text-light: var(--ons-color-grey-75);
+	--ons-color-text-inverse: var(--ons-color-white);
+	--ons-color-text-banner: var(--ons-color-black);
+	--ons-color-text-link: var(--ons-color-ocean-blue);
+	--ons-color-text-link-hover: var(--ons-color-night-blue);
+	--ons-color-text-link-active: var(--ons-color-night-blue);
+	--ons-color-text-link-focus: var(--ons-color-black);
+	--ons-color-text-inverse-link: var(--ons-color-white);
+	--ons-color-text-inverse-link-hover: var(--ons-color-grey-5);
+	--ons-color-text-banner-link: var(--ons-color-grey-100);
+	--ons-color-text-banner-link-hover: var(--ons-color-black);
+	--ons-color-text-metadata: var(--ons-color-black);
+	--ons-color-focus: var(--ons-color-sun-yellow);
+	--ons-color-focus-dark: var(--ons-color-sun-yellow-dark);
+	--ons-color-highlight: var(--ons-color-neon-yellow);
+	--ons-color-borders: var(--ons-color-grey-75);
+	--ons-color-borders-indent: var(--ons-color-grey-35);
+	--ons-color-borders-list: var(--ons-color-grey-35);
+	--ons-color-borders-document-image: var(--ons-color-grey-15);
+	--ons-color-borders-document-image-focus: var(--ons-color-black);
+	--ons-color-placeholder: var(--ons-color-grey-15);
+	--ons-color-button: var(--ons-color-leaf-green);
+	--ons-color-button-secondary: var(--ons-color-grey-15);
+	--ons-color-button-shadow: var(--ons-color-leaf-green-dark-10);
+	--ons-color-button-hover: var(--ons-color-leaf-green-dark-5);
+	--ons-color-button-secondary-shadow: var(--ons-color-grey-75);
+	--ons-color-button-secondary-hover: var(--ons-color-grey-25);
+	--ons-color-input-border: var(--ons-color-black);
+	--ons-color-input-bg: var(--ons-color-white);
+	--ons-color-info: var(--ons-color-ocean-blue);
+	--ons-color-info-tint: var(--ons-color-ocean-blue-tint);
+	--ons-color-info-vibrant: var(--ons-color-ocean-blue-vibrant);
+	--ons-color-success: var(--ons-color-leaf-green);
+	--ons-color-success-tint: var(--ons-color-leaf-green-tint);
+	--ons-color-success-vibrant: var(--ons-color-leaf-green-vibrant);
+	--ons-color-errors: var(--ons-color-ruby-red);
+	--ons-color-errors-tint: var(--ons-color-ruby-red-tint);
+	--ons-color-errors-vibrant: var(--ons-color-ruby-red-vibrant);
+	--ons-color-pending: var(--ons-color-jaffa-orange);
+	--ons-color-pending-tint: var(--ons-color-jaffa-orange-tint);
+	--ons-color-pending-vibrant: var(--ons-color-jaffa-orange-vibrant);
+	--ons-color-dead: var(--ons-color-grey-75);
+	--ons-color-instruction: var(--ons-color-jaffa-orange);
+	--ons-color-instruction-tint: var(--ons-color-jaffa-orange-tint);
+	--ons-color-text-disabled: var(--ons-color-grey-35);
+	--ons-color-border-disabled: var(--ons-color-grey-35);
+}
 </style>
