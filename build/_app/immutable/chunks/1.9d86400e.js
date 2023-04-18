a53d8d2220c84ea2404480cab44917bb0286307a
henryjameslau <henry.james.lau@gmail.com>
2023-04-18 12:20:54 +0100

    why doesn't this get added?

---
+++ build/_app/immutable/chunks/1.9d86400e.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.ae337ba0.js";export{t as component};
---
+++ build/_app/immutable/chunks/2.a4c2b4fd.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.c2dc96ce.js";export{t as component};
