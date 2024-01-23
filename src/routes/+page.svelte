ba99ecfea71733e6c825a0101f74fd4a6476bbf5
henryjameslau <henry.james.lau@gmail.com>
2024-01-23 11:09:25 +0000

    Handle no price items
    But they still have growth rates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -90,7 +90,7 @@
 		columns=[
 		{label:"Name (weight or size)",prop:"Name",sort:true,type:"text"},
 		// {label:"Weight or size",prop:"Weight or size",sort:false,type:"text",formatFn:(d)=> d=='null' ? '' : d},
-		{label:"Average price",prop:"Average price",sort:true,type:"number",formatFn:(d)=>'£'+format(',.2f')(d)},
+		{label:"Average price",prop:"Average price",sort:true,type:"number",formatFn:(d)=>d==null ? 'Unavailable' : '£'+format(',.2f')(d)},
 		{label:"Annual growth",prop:"Annual growth",sort:true,type:"number",formatFn:(d)=>d==null ? 'Unavailable' : format('.0f')(d)+'%'}
 		]
 	}else{
